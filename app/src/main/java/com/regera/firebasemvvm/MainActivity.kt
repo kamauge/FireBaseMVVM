2eed9b1b3e977489f6d0ecc2b36109fddf6497bb
kamauge <kamaugerald2@gmail.com>
2023-01-08 17:39:26 +0300

    added navigation component

---
+++ app/src/main/java/com/regera/firebasemvvm/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.regera.firebasemvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var user : MutableMap<String,Any> = HashMap()
+        user["first"] = "Bruce"
+        user["last"] = "Wayne"
+        user["born"] = 1994
+
+        FirebaseFirestore.getInstance().collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d("TAG",documentReference.id)
+
+            }
+            .addOnFailureListener {
+                 e -> Log.w("TAG","Error adding document",e)
+            }
+
+
+    }
+}
